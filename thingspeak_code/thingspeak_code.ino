a09f75a61fb5187694f632ca406951bad7b88c4d
vorasit phusuwan <40907778+vorasit@users.noreply.github.com>
2021-03-22 02:48:23 +0700

    thingspeak
    esp8266+thingspeak

---
+++ thingspeak_code/thingspeak_code.ino
@@ -0,0 +1,52 @@
+#include <DHT.h>
+#include <ESP8266WiFi.h>
+//กรณีเชื่อมต่อแล้วแต่ไม่ส่งค่าเป็นที่สัญญาณเครือข่ายอาทิ ทรู เอไอเอส
+String apiKey = "N8ULXVRNIUSMWFRP"; // thingspeak API key ที่ได้จากการสร้างของผู้ใช้
+const char* ssid = "B4-12_2G";
+const char* password = "12345678";
+const char* server = "api.thingspeak.com";
+#define DHTPIN D2
+#define DHTTYPE DHT22 // DHT11 if useDHT11
+DHT dht(DHTPIN, DHTTYPE);
+WiFiClient client;
+void setup() {
+Serial.begin(115200);
+delay(10);
+dht.begin();
+WiFi.begin(ssid, password);
+Serial.println();
+Serial.println();
+Serial.print("Connecting to ");
+Serial.println(ssid);
+WiFi.begin(ssid, password);
+while (WiFi.status() != WL_CONNECTED) {
+delay(500);
+Serial.print(".");
+}
+Serial.println("");
+Serial.println("WiFi connected");
+}
+void loop() {
+float h = dht.readHumidity();
+float t = dht.readTemperature();
+if (isnan(h) || isnan(t)) {
+Serial.println("Failed to read from DHT sensor!");
+return;
+}
+if (client.connect(server,80)) {
+String url = "/update?api_key="+ apiKey;
+url += "&field1=" + (String)t;
+url += "&field2=" + (String)h;
+client.print(String("GET ") + url + " HTTP/1.1\r\n" +
+"Host: " + server + "\r\n" +
+"Connection: close\r\n\r\n");
+Serial.print("Temp: ");
+Serial.print(t);
+Serial.print(" *C and Humidity: ");
+Serial.print(h);
+Serial.println("% ");
+}
+client.stop();
+Serial.print("Waiting...");
+delay(15000); // thingspeak needs minimum 15 sec delay between updates
+}
